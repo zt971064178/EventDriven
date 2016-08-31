0d633d9804354b194d62ce829356b6803ce659f5
zhangtian <zhangtian@happy>
2016-08-31 21:30:05 +0800

    lambdaj测试

--- base-event-driven/src/test/java/cn/itcast/lambdaj/collection/test/LambdajBaseTest.java
+++ base-event-driven/src/test/java/cn/itcast/lambdaj/collection/test/LambdajBaseTest.java
@@ -1,10 +1,19 @@
 package cn.itcast.lambdaj.collection.test;
 
 import java.util.ArrayList;
+import ch.lambdaj.function.closure.Closure; 
+import static ch.lambdaj.Lambda.*;
+import static ch.lambdaj.collection.LambdaCollections.*;
+import static org.hamcrest.Matchers.*;
 import java.util.List;
+import java.util.Map;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 import cn.itcast.lambdaj.collection.Person;
 
@@ -27,9 +36,66 @@ public class LambdajBaseTest {
 		persons.add(p);
 	}
 	
+	/*
+	 *  joinFrom（连接字段）
+	 */
 	@Test
 	public void testJoinFrom() {
-		
+		String names = joinFrom(persons).getName() ;
+		System.out.println(names);
+		names = joinFrom(persons, "==").getName() ;
+		System.out.println(names);
 	}
 	
+	/*
+	 * select(条件选择)
+	 * selectMax,selectMin(最大/最小 对象)
+	 */
+	@Test
+	public void testSelect() {
+		// 筛选出年龄大于33岁的人
+		List<Person> ageGreaterThan33 = select(persons,having(on(Person.class).getAge(),Matchers.greaterThan(33)));
+		System.out.println(JSONArray.toJSONString(ageGreaterThan33));
+		
+		Person personWithMaxAge = selectMax(persons, on(Person.class).getAge());// 得到年龄最大的人
+		System.out.println(JSONObject.toJSONString(personWithMaxAge));
+	}
+	
+	/*
+	 * max,min(最大/最小 对象属性值)
+	 * maxFrom,minFrom(和max,min功能一样)
+	 * sum,sunFrom(求和)
+	 */
+	@Test
+	public void testMinMax() {
+		int maxAge = max(persons, on(Person.class).getAge()) ;
+		System.out.println(maxAge);
+		
+		maxAge = maxFrom(persons).getAge() ;
+		System.out.println(maxAge);
+		
+		int minAge = minFrom(persons).getAge() ;
+		System.out.println(minAge);
+		
+		System.out.println(sumFrom(persons).getAge());
+		System.out.println(sum(persons, on(Person.class).getAge()));
+	}
+	
+	// sort(排序) extract(抽取字段属性组成集合)
+	@Test
+	public void testSortExtra() {
+		System.out.println(JSONArray.toJSONString(persons));
+		List<Person> sortsP = sort(persons, on(Person.class).getAge()) ;
+		System.out.println(JSONArray.toJSONString(sortsP));
+	
+		List<Integer> ageList = extract(persons, on(Person.class).getAge());
+		System.out.println(JSONArray.toJSONString(ageList));
+	}
+	
+	// 以某字字段属性为关键值分组
+	@Test
+	public void indexP() {
+		Map<String,Person> mapByName = index(persons, on(Person.class).getName()) ;
+		System.out.println(JSONObject.toJSONString(mapByName));
+	}
 }
